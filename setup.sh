a9547087346f2000970453a076c3742dd72ec606
louisscruz <louisstephancruz@me.com>
2017-10-11 21:41:55 -0700

    Fix typo

--- setup.sh
+++ setup.sh
@@ -128,7 +128,7 @@ install_rbenv() {
     alert_installed rbenv
   fi
 
-  echo "export PATH="$HOME/.rbenv/bin:$PATH"" >> ~/.bash_profile
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 
   if [[ ! -f ~/.rbenv ]]; then
     echo "if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi" >> ~/.bash_profile
