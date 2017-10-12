72d25d0b48b1cb31ce70943a4c1c3f592c310eff
louisscruz <louisstephancruz@me.com>
2017-10-11 19:54:29 -0700

    Add remote commands

--- remote_setup.sh
+++ remote_setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+[[ -x `command -v wget` ]] && CMD="wget --no-check-certificate -O -"
+[[ -x `command -v curl` ]] >/dev/null 2>&1 && CMD="curl -#L"
+
 if [ -z "$CMD" ]; then
   echo "No curl or wget available"
   echo "Aborting"
